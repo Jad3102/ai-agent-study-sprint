9dcdbb9564dd87632a45a98bcd5934ec0fcef464
jad3102 <juliaalvesdorneles2@gmail.com>
2026-05-04 11:44:25 -0300

    feat(redis): cache de queries do PostgreSQL com Redis

--- api/index.js
+++ api/index.js
@@ -18,11 +18,31 @@ const pool = new Pool({
   ssl: false,
   password_encryption: 'scram-sha-256'
 })
-console.log('senha:', process.env.DB_PASSWORD);
-console.log('user:', process.env.DB_USER);
+
+//Pra conectar com o Redis
+
+import { createClient } from 'redis'
+
+const redis = createClient()
+redis.on('error', err => console.log('Redis error:', err))
+await redis.connect()
+console.log('Redis conectado!')
+
 //pra buscar users (GET)
 app.get('/usuarios', async (req, res) => {
+  const cache = await redis.get('usuarios');
+
+  //verifica se já existe no cache
+  if (cache) {
+    console.log('retornando do cache!')
+    return res.json(JSON.parse(cache))
+  }
+  //se não existe, ele salva durante 30s
   const resultado = await pool.query('SELECT * FROM usuarios')
+  await redis.set('usuarios', JSON.stringify(resultado.rows), { EX: 30 })
+
+  //avisa se vem do banco
+  console.log('retornando do banco!')
   res.json(resultado.rows)
 })
 
@@ -33,6 +53,9 @@ app.post('/usuarios', async (req, res) => {
     'INSERT INTO usuarios (nome, email) VALUES ($1, $2) RETURNING *',
     [nome, email]
   )
+
+  // limpa o cache pra forçar nova consulta no banco
+  await redis.del('usuarios')
   res.json(resultado.rows[0])
 })
 
@@ -44,6 +67,7 @@ app.put('/usuarios/:id', async (req, res) => {
     'UPDATE usuarios SET nome=$1, email=$2 WHERE id=$3 RETURNING *',
     [nome, email, id]
   )
+
   res.json(resultado.rows[0])
 })
 
@@ -53,6 +77,8 @@ app.delete('/usuarios/:id', async (req, res) => {
   await pool.query('DELETE FROM usuarios WHERE id=$1', [id])
   res.json({ mensagem: 'usuário deletado com sucesso' })
 })
+// limpa o cache
+  await redis.del('usuarios')
 
 app.listen(3000, () => {
   console.log('API rodando na porta 3000')
